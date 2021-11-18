919c5f9f3d00b800b2da050ab2cc4156fd9f458f
Georgek Aroush <georgek.aroush@gmail.com>
2021-11-19 00:42:29 +0100

    Setup files for Electron app, using Vue3, Vite and TailwindCSS

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <HelloWorld class="text-center m-14 text-blue-800" msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
